b662d0a6333af54c5091d8d99139105ef608e19b
Slathian19 <papray1538@gmail.com>
2025-06-16 09:00:15 +0000

    update code

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "sininat"
+last_name = "Lathiang"
+print(first_name + " " + last_name)
+
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "sininat"
+last_name = "Lathiang"
+whole_name = first_name + " " + last_name
+print(whole_name)

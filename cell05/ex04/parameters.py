e8f3a4673f8c6823508763b6256fc57d2ef858fe
Slathian19 <papray1538@gmail.com>
2025-06-18 12:47:08 +0000

    update

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,4 +1,4 @@
 import sys
-number_of_parameters = len(sys.argv) - 1
 
-print(f"Number of parameters: {number_of_parameters}.")
+num_parameter = len(sys.argv) - 1
+print(f"Number of parameters: {num_parameter}")
